c01eed72f22b1c81e7295c642a0aead0e457bf2e
rishurajgupta <rishurajgupta@gmail.com>
2018-09-13 18:39:00 +0530

    firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyACumuVSR2RlY3uvbLh2rCAs9ecB_VwtGk",
-    authDomain: "cim-angular.firebaseapp.com",
-    databaseURL: "https://cim-angular.firebaseio.com",
-    projectId: "cim-angular",
-    storageBucket: "cim-angular.appspot.com",
-    messagingSenderId: "368894646465"
+    apiKey: "AIzaSyClmLpyOcgH6Xt_qadmFgyT5Vw4RwXYZ80",
+    authDomain: "tailor-marker.firebaseapp.com",
+    databaseURL: "https://tailor-marker.firebaseio.com",
+    projectId: "tailor-marker",
+    storageBucket: "tailor-marker.appspot.com",
+    messagingSenderId: "210769403259"
   }
 };
